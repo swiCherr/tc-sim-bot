582017b972496251014ce27b0431af1da86cc480
swiCherr <139969039+swiCherr@users.noreply.github.com>
2023-07-19 13:38:55 +0300

    s
    s

---
+++ Kurulum Talimatları.md
@@ -0,0 +1,19 @@
+## 1 - İnidiyorsunuz Node.js LTS sürümü site https://nodejs.org/en/
+
+## 2 - https://discord.dev Botunuzu Açın
+
+## 3 - https://discordapi.com/permissions.html Botunuzun Davet Linkini Alın
+
+## 4 - ayarlar.json Dosyasını Eksiksiz Doldur.
+
+## 6 - Başlat.bat tıkladık'dan sonra çalışacaktır 
+
+
+
+
+Kurulumu Yapamazsanız max 1-48 saat icinde yardıma gelicez merak etmeyin kurulumu çok basit
+
+
+## Destek
+
+Discord: [benjamaris](https://discord.com/users/889207617933676555)
---
+++ bot.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js')
+const { Client, Partials } = require('discord.js')
+const client = new Client({ intents: 131071, partials: Object.values(Partials).filter(x => typeof x === "string"), shards: 'auto' })
+const { botid, token } = require("./ayarlar.json")
+require("./slash")(client)
+
+client.on('messageCreate', async message => {
+
+    const embedetiket = new Discord.EmbedBuilder()
+    .setColor('Blue')
+    .setDescription(`**/yardım** Komutu ile Komutlarımı Görebilirsiniz`)
+    if (message.content === `<@${botid}>`) {
+      message.channel.send({ embeds: [embedetiket] })
+    }
+
+})
+
+const { GiveawaysManager } = require('discord-giveaways')
+const manager = new GiveawaysManager(client, {
+    storage: './giveaways.json',
+    default: {
+        botsCanWin: false,
+        embedColor: 'Green',
+        embedColorEnd: 'Grey',
+        reaction: '🎉'
+    }
+})
+client.giveawaysManager = manager
+
+client.login(token)
